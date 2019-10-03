7022807735b1a237aaac883cc9df3b970c9dc1a3
c4m <c4m@10.31.56.200>
2019-10-03 16:10:28 -0400

    yeet

--- Assignment1/src/PayRoll.java
+++ Assignment1/src/PayRoll.java
@@ -3,14 +3,21 @@ import java.io.FileNotFoundException;
 import java.util.Scanner; 
 
 public class PayRoll {
+	
+	
+	
 
 		public static void main(String args[]) {
-			Scanner sc = new Scanner(System.in);
+			Scanner lineScan = new Scanner("employee.txt");
+			
+			ArrayList<Employee> Employees = new ArrayList<Employee>();
+			
+			
 			
 			System.out.println("In what mode would you like to create the pay checks? Enter W for Weekly, M for monthly or X to exit\r\n" + 
 					"the system.");
 			
-			String act = sc.nextLine();
+			String act.equals(sc.nextLine();
 			
 			if (act == "W") {
 			PartTime PT1 = new PartTime();
